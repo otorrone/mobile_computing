b699b93deb7a944a09aedbed61c514619c06c52a
otorrone <olli.olapade@hotmail.com>
2020-02-04 08:53:09 +0200

    Time activity update (broken)

--- app/src/main/java/com/example/myapplication/TimeActivity.kt
+++ app/src/main/java/com/example/myapplication/TimeActivity.kt
@@ -2,11 +2,38 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import java.util.*
 
 class TimeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time)
+
+        time_create.setOnClickListener{
+            val calendar = GregorianCalendar(
+                datePicker.year,
+                datePicker.month,
+                datePicker.dayOfMonth,
+                datePicker.currentHour,
+                datePicker.curreentMinute
+            )
+            val reminder = Reminder (
+                uid = null,
+                time = calendar.timeInMillis,
+                location = null,
+                message = et_message.text.toString()
+            )
+
+            doAsync (
+
+                val db = Rooom.databaseBuilder(applicationContext, AppDatabase::class.java, "Reminders").build()
+                db.reminderDao().insert(reminder)
+                db.close()
+
+                finish()
+            )
+        }
     }
 }
